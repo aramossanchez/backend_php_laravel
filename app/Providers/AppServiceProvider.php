0827bad452b8d8057b5a79068f8a75a0b4ccb73c
Armando <armandoramossanchez@gmail.com>
2021-12-09 13:28:41 +0100

    cambios en AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,9 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // if(env('REDIRECT_HTTPS')){
-        //     $this->app['request']->server-set('HTTPS', true);
-        // }
+        if(env('REDIRECT_HTTPS')){
+            $this->app['request']->server-set('HTTPS', true);
+        }
     }
 
     /**

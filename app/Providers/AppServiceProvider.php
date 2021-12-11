0ce960636482366ca5e69add8412b04597a974ff
Armando <armandoramossanchez@gmail.com>
2021-12-11 19:00:29 +0100

    subo correccion para deploy en heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')){
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**

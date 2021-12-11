a43a9d99555de1f6e4a434314ccf92047e7f8241
Armando <armandoramossanchez@gmail.com>
2021-12-11 19:07:41 +0100

    subo correccion para deploy en heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,5 +30,6 @@ class AppServiceProvider extends ServiceProvider
         if(env('REDIRECT_HTTPS')){
             $url->formatScheme('https://');
         }
+        Schema::defaultStringLength(191);
     }
 }

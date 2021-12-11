8daa369dc23cfd2e4fc7290b23f73e79f182aef2
Armando <armandoramossanchez@gmail.com>
2021-12-11 18:33:37 +0100

    subo correccion para deploy en heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,10 +24,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
-    {
-        if(env('REDIRECT_HTTPS')){
-            $url->formatScheme('https://');
-        }
-    }
+    // public function boot(UrlGenerator $url)
+    // {
+    //     if(env('REDIRECT_HTTPS')){
+    //         $url->formatScheme('https://');
+    //     }
+    // }
 }
